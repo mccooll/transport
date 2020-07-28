f4ccc643cda23d3bf93abd0551f2c095ba6ad3d3
mccooll <github@no3.ca>
2020-07-28 16:51:44 -0400

    get itinerary

--- src/routes/itineraries/index.js
+++ src/routes/itineraries/index.js
@@ -2,37 +2,83 @@ import { h, Component } from 'preact';
 
 export default class Itineraries extends Component {
 
+	state = {
+		itinerary: []
+	}
+
+	componentDidMount() {
+		this.itinerary();
+	}
+
+	itinerary() {
+		if(this.props.state.flights.length < 1 || this.props.state.orders.length < 1) {
+			return [];
+		}
+		const flights = [...this.props.state.flights];
+		const maxCapacity = 20;
+		flights.forEach(flight => flight.capacity = maxCapacity);
+		const orders = [...this.props.state.orders];
+		let itinerary = []
+		orders.forEach((order) => {
+			let slotted = false;
+			let flightNumber = 0;
+			do {
+				console.log(flightNumber)
+				let flight = flights[flightNumber];
+				
+				//check for departure && arrival
+				if(flight.arrival !== order.destination) continue;
+				//check for capacity
+				if(flight.capacity < 1) continue;
+				
+				let slot = {...flight};
+				slot.order = order.number;
+				slot.flight = flightNumber;
+				itinerary.push(slot);
+				flight.capacity--;
+				slotted = true;
+			} while(!slotted && flights.length > ++flightNumber)
+			if(!slotted) {
+				let slot = {};
+				slot.order = order.number;
+				slot.flight = "not scheduled";
+				itinerary.push(slot);
+			}
+		})
+		this.setState({itinerary: itinerary});
+	}
+
 	render() {
-		return (
-			<div>
-				<h1>Itineraries</h1>
-				<table>
-					<tr>
-						<th>Order</th>
-						<th>Flight Number</th>
-						<th>From</th>
-						<th>To</th>
-						<th>Day</th>
-					</tr>
-					<tr>
-						<td>001</td>
-						<td>1</td>
-						<td>YUL</td>
-						<td>YYZ</td>
-						<td>1</td>
-					</tr>
-					<tr>
-						<td>001</td>
-						<td>1</td>
-						<td>YUL</td>
-						<td>YYZ</td>
-						<td>1</td>
-					</tr>
-				</table>
+		if(this.state.itinerary.length > 0) {
+			return (
+				<div>
+					<h1>Itineraries</h1>
+					<table>
+						<tr>
+							<th>Order</th>
+							<th>Flight Number</th>
+							<th>From</th>
+							<th>To</th>
+							<th>Day</th>
+						</tr>
+						{this.state.itinerary.map((slot) => (
+							<tr>
+								<td>{slot.order}</td>
+								<td>{slot.flight}</td>
+								<td>{slot.departure}</td>
+								<td>{slot.arrival}</td>
+								<td>{slot.day}</td>
+							</tr>
+						))}
+					</table>
+				</div>
+			);
+		} else {
+			return(
 				<p>
 					You need to upload orders.
 				</p>
-			</div>
-		);
+			);
+		}
 	}
 }
